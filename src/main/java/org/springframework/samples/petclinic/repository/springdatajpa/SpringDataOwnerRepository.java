ddd4ba7375528285a1986d2f0eeac94bfff1190c
kalibek-epam <127488307+kalibek-epam@users.noreply.github.com>
2024-06-08 18:28:32 +0500

    Update src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataOwnerRepository.java

--- src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataOwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataOwnerRepository.java
@@ -26,16 +26,16 @@ import org.springframework.samples.petclinic.repository.OwnerRepository;
 /**
  * Spring Data JPA specialization of the {@link OwnerRepository} interface
  *
- * @author Michael Isvy
+ * Michael Isvy
  * @since 15.1.2013
  */
 public interface SpringDataOwnerRepository extends OwnerRepository, Repository<Owner, Integer> {
 
     @Override
     @Query("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.lastName LIKE :lastName%")
-    public Collection<Owner> findByFirstName(@Param("lastName") String lastName);
+    Collection<Owner> findByLastName(@Param("lastName") String lastName);
 
     @Override
     @Query("SELECT owner FROM Owner owner left join fetch owner.pets WHERE owner.id =:id")
-    public Owner findById(@Param("id") int id);
+    Owner findById(@Param("id") int id);
 }
