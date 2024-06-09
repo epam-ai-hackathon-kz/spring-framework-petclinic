19306c6cb99ad7ee746a019885852c9a1206c688
Eugene Svalukhin <eugene.svalukhin@gmail.com>
2024-06-09 15:24:25 +0500

    Update src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
@@ -16,15 +16,18 @@
 package org.springframework.samples.petclinic.repository.jpa;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 
 import org.springframework.orm.hibernate5.support.OpenSessionInViewFilter;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.repository.OwnerRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * JPA implementation of the {@link OwnerRepository} interface.
@@ -36,34 +39,28 @@ import org.springframework.stereotype.Repository;
  * @since 22.4.2006
  */
 @Repository
+@Transactional
 public class JpaOwnerRepositoryImpl implements OwnerRepository {
 
     @PersistenceContext
     private EntityManager em;
 
-    /**
-     * Important: in the current version of this method, we load Owners with all their Pets and Visits while
-     * we do not need Visits at all and we only need one property from the Pet objects (the 'name' property).
-     * There are some ways to improve it such as:
-     * - creating a Lightweight class (example here: https://community.jboss.org/wiki/LightweightClass)
-     * - Turning on lazy-loading and using {@link OpenSessionInViewFilter}
-     */
-    @SuppressWarnings("unchecked")
+    @Override
     public Collection<Owner> findByLastName(String lastName) {
-        // using 'join fetch' because a single query should load both owners and pets
-        // using 'left join fetch' because it might happen that an owner does not have pets yet
-        Query query = this.em.createQuery("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.lastName LIKE :lastName");
+        TypedQuery<Owner> query = this.em.createQuery(
+                "SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.lastName LIKE :lastName", 
+                Owner.class);
         query.setParameter("lastName", lastName + "%");
         return query.getResultList();
     }
 
     @Override
-    public Owner findById(int id) {
-        // using 'join fetch' because a single query should load both owners and pets
-        // using 'left join fetch' because it might happen that an owner does not have pets yet
-        Query query = this.em.createQuery("SELECT owner FROM Owner owner left join fetch owner.pets WHERE owner.id =:id");
+    public Optional<Owner> findById(int id) {
+        TypedQuery<Owner> query = this.em.createQuery(
+                "SELECT owner FROM Owner owner left join fetch owner.pets WHERE owner.id =:id", 
+                Owner.class);
         query.setParameter("id", id);
-        return (Owner) query.getSingleResult();
+        return Optional.ofNullable(query.getSingleResult());
     }
 
     @Override
@@ -75,4 +72,21 @@ public class JpaOwnerRepositoryImpl implements OwnerRepository {
         }
     }
 
+    @Override
+    public void delete(Owner owner) {
+        if (this.em.contains(owner)) {
+            this.em.remove(owner);
+        } else {
+            Owner managedOwner = this.em.merge(owner);
+            this.em.remove(managedOwner);
+        }
+    }
+
+    @Override
+    public Collection<Owner> findAll() {
+        TypedQuery<Owner> query = this.em.createQuery(
+                "SELECT owner FROM Owner owner left join fetch owner.pets", 
+                Owner.class);
+        return query.getResultList();
+    }
 }
