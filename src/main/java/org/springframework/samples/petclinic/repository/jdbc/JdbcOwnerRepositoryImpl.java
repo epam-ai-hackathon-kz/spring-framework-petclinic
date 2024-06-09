8b19da984b13f7449fb998c28665137e398b5414
arman-baikenzhin <arman.baikenzhin@ah.nl>
2024-06-09 15:27:38 +0500

    Update src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryImpl.java

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryImpl.java
@@ -15,7 +15,10 @@
  */
 package org.springframework.samples.petclinic.repository.jdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -48,6 +51,8 @@ import java.util.List;
 @Repository
 public class JdbcOwnerRepositoryImpl implements OwnerRepository {
 
+    private static final Logger logger = LoggerFactory.getLogger(JdbcOwnerRepositoryImpl.class);
+
     private final JdbcClient jdbcClient;
 
     private final SimpleJdbcInsert insertOwner;
@@ -63,11 +68,6 @@ public class JdbcOwnerRepositoryImpl implements OwnerRepository {
 
     }
 
-    /**
-     * Loads {@link Owner Owners} from the data store by last name, returning all owners whose last name <i>starts</i> with
-     * the given name; also loads the {@link Pet Pets} and {@link Visit Visits} for the corresponding owners, if not
-     * already loaded.
-     */
     @Override
     public Collection<Owner> findByLastName(String lastName) {
         List<Owner> owners = this.jdbcClient.sql("""
@@ -82,10 +82,6 @@ public class JdbcOwnerRepositoryImpl implements OwnerRepository {
         return owners;
     }
 
-    /**
-     * Loads the {@link Owner} with the supplied <code>id</code>; also loads the {@link Pet Pets} and {@link Visit Visits}
-     * for the corresponding owner, if not already loaded.
-     */
     @Override
     public Owner findById(int id) {
         Owner owner;
@@ -122,17 +118,22 @@ public class JdbcOwnerRepositoryImpl implements OwnerRepository {
     @Override
     public void save(Owner owner) {
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(owner);
-        if (owner.isNew()) {
-            Number newKey = this.insertOwner.executeAndReturnKey(parameterSource);
-            owner.setId(newKey.intValue());
-        } else {
-            this.jdbcClient.sql("""
-                    UPDATE owners
-                    SET first_name=:firstName, last_name=:lastName, address=:address, city=:city, telephone=:telephone
-                    WHERE id=:id
-                    """)
-                .paramSource(parameterSource)
-                .update();
+        try {
+            if (owner.isNew()) {
+                Number newKey = this.insertOwner.executeAndReturnKey(parameterSource);
+                owner.setId(newKey.intValue());
+            } else {
+                this.jdbcClient.sql("""
+                        UPDATE owners
+                        SET first_name=:firstName, last_name=:lastName, address=:address, city=:city, telephone=:telephone
+                        WHERE id=:id
+                        """)
+                    .paramSource(parameterSource)
+                    .update();
+            }
+        } catch (Exception ex) {
+            logger.error("Failed to save owner: " + owner, ex);
+            throw new DataAccessException("Failed to save owner: " + owner, ex) {};
         }
     }
 
@@ -142,12 +143,6 @@ public class JdbcOwnerRepositoryImpl implements OwnerRepository {
             .list();
     }
 
-    /**
-     * Loads the {@link Pet} and {@link Visit} data for the supplied {@link List} of {@link Owner Owners}.
-     *
-     * @param owners the list of owners for whom the pet and visit data should be loaded
-     * @see #loadPetsAndVisits(Owner)
-     */
     private void loadOwnersPetsAndVisits(List<Owner> owners) {
         for (Owner owner : owners) {
             loadPetsAndVisits(owner);
